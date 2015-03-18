8cb941ec4d4ed2331dd11881be0fc45268363272
jiliang <jiliang@Jiliang-ThinkPad-T420>
2015-03-18 03:23:10 -0700

    ok

--- src/main/java/edu/gqq/thread/noticeandwait/RandomTest.java
+++ src/main/java/edu/gqq/thread/noticeandwait/RandomTest.java
@@ -6,22 +6,29 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+
 import static java.lang.System.out;
 import edu.gqq.common.RandomUtil;
+import edu.gqq.util.Log4jUtil;
 
 class ModifyList {
+	static Logger logger = Log4jUtil.getLogger(ModifyList.class);
 	ArrayList<Integer> arrs = new ArrayList<Integer>();
 
 	public synchronized void add(int e) {
+		if (arrs.size() >= 10)
+			arrs.clear();
 		arrs.add(e);
 	}
 
 	public synchronized void print() {
-		arrs.forEach(x -> out.println(x));
+		arrs.forEach(x -> logger.debug(x));
 	}
 }
 
 class Process extends Thread {
+	static Logger logger = Log4jUtil.getLogger(Process.class);
 	ModifyList m;
 	int value;
 
@@ -31,14 +38,15 @@ class Process extends Thread {
 	}
 	@Override
 	public void run() {
-		out.println("start to process...");
+		logger.debug("start to process...");
 		// Arrays.asList(5, 7, 8, 2).forEach(x -> out.print(x + " "));
 		m.add(value);
-		out.println("process ok...");
+		logger.debug("process ok...");
 	}
 }
 
 public class RandomTest extends Thread {
+	static Logger logger = Log4jUtil.getLogger(RandomTest.class);
 	ModifyList m = new ModifyList();
 	@Override
 	public void run() {
@@ -47,23 +55,23 @@ public class RandomTest extends Thread {
 			int randInt = RandomUtil.randInt(1, 10);
 
 			if (randInt <= 7) {
-				out.println(getName() + "---" + randInt + " start to process");
+				logger.debug(getName() + "---" + randInt + " start to process");
 				try {
 					// TimeUnit.SECONDS.sleep(1);
 					Process p = new Process(m, randInt);
 					p.start();
 					// p.join();
-					out.println("process end, " + getName() + " invoked");
+					logger.debug("process end, " + getName() + " invoked");
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			} else {
-				out.println(getName() + "---" + randInt + " will sleep 10s\n");
+				logger.debug(getName() + "---" + randInt + " will sleep 10s\n");
 				try {
 					TimeUnit.SECONDS.sleep(5);
 					m.print();
-					out.println("sleep end, " + getName() + " invoked");
+					logger.debug("sleep end, " + getName() + " invoked");
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -74,6 +82,7 @@ public class RandomTest extends Thread {
 	}
 
 	public static void main(String[] args) {
+		logger.debug("starting...");
 		new RandomTest().start();
 	}
 }
