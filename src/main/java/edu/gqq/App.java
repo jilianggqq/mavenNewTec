e6019ceeb7970d17f68a8cbfc6d6daccf95f28af
Peter X230 <PeterX230@itu.edu>
2015-09-20 15:46:39 -0700

    gradle

--- src/main/java/edu/gqq/App.java
+++ src/main/java/edu/gqq/App.java
@@ -1,11 +1,18 @@
 package edu.gqq;
 
+import org.apache.log4j.Logger;
+
+import edu.gqq.util.Log4jUtil;
+
 /**
  * Hello world!
  *
  */
 public class App {
+	static Logger logger = Log4jUtil.getLogger(App.class);
+
 	public static void main(String[] args) {
+		logger.info("this is logger info");
 		System.out.println("Hello World!");
 	}
 }
