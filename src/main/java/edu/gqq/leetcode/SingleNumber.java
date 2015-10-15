de83a6083f03a193ed64b41b8c33e97c7ed41321
Peter X230 <PeterX230@itu.edu>
2015-10-15 15:59:44 -0700

    leetcode xor

---
+++ src/main/java/edu/gqq/leetcode/SingleNumber.java
@@ -0,0 +1,30 @@
+package edu.gqq.leetcode;
+
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+
+import edu.gqq.util.Log4jUtil;
+
+public class SingleNumber {
+	static Logger logger = Log4jUtil.getLogger(SingleNumber.class);
+
+	int result;
+
+	public int singleNumber(int[] nums) {
+		// you must understand xor
+		// n^n=0, n^0=n
+		Arrays.stream(nums).forEach(x -> result ^= x);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		logger.debug(4 ^ 4);
+		logger.debug(4 ^ 3);
+		logger.debug(4 ^ 0);
+		logger.debug(114 ^ 0);
+		logger.debug(4 ^ 3 ^ 4 ^ 3);
+		int[] a = {1,1,2,2,3};
+		logger.debug(new SingleNumber().singleNumber(a));
+	}
+}
