a1978453077fabd8a1492ff8899a3f48b3a3bf2b
peter BigPc ubuntu <peter_BigPc_ubuntu@itu.edu>
2016-09-25 16:24:35 -0700

    a demo for kmp algorithm

---
+++ src/main/java/edu/gqq/algorithms/KMPDemo.java
@@ -0,0 +1,75 @@
+package edu.gqq.algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 且当前字符匹配失败（即S[i] != P[j]），则令 i 不变，j = next[j]
+ * 
+ * @author peter
+ *
+ */
+public class KMPDemo {
+
+	/**
+	 * this method should be recited.
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public int[] getNextTable(String s) {
+		int len = s.length();
+		int[] next = new int[len];
+		int k = -1;
+		next[0] = -1;
+		int j = 0;
+		while (j < len - 1) {
+			if (k == -1 || s.charAt(j) == s.charAt(k)) {
+				++k;
+				++j;
+				next[j] = next[k] + 1;
+			} else {
+				k = next[k];
+			}
+		}
+		return next;
+	}
+
+	public int getFirstIndex(String des, String src) {
+		int len1 = src.length();
+		int len2 = des.length();
+		int[] next = getNextTable(des);
+		int i = 0, j = 0;
+		while (i < len1 && j < len2) {
+			System.out.println(String.format("{i:%s, j:%s}", i, j));
+			if (j == -1 || src.charAt(i) == des.charAt(j)) {
+				++i;
+				++j;
+			} else {
+				j = next[j];
+			}
+		}
+		if (j == len2) {
+			return i - j;
+		}
+		return -1;
+	}
+
+
+//	@Test
+//	public void testKMPDemo() throws Exception {
+//		int[] des = { -1, 0, 0, 0, 1 };
+//		assertArrayEquals(des, getNextTable("abcab"));
+//		int[] des2 = { -1, 0, 0, 0, 0, 1, 2 };
+//		assertArrayEquals(des2, getNextTable("ABCDABD"));
+//	}
+	
+	@Test
+	public void testStringMatch() throws Exception {
+		String str1 = "bbcabcdababcdabcdabde";
+		String str2 = "abcdabd";
+		int index = getFirstIndex(str2, str1);
+		assertEquals(14, index);
+	}
+}
